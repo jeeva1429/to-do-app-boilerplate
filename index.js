eba36b83f57c2fa52e858001c2b26887055d11aa
jeeva parkkavan <jeeva.parkkavan@kalvium.community>
2023-02-24 06:30:04 +0530

    changes made in the index.js and index.html files

---
+++ index.js
@@ -0,0 +1,41 @@
+var addButton = document.getElementById('button')
+var toDoList = document.getElementById('todolist')
+
+
+// This function will get the user input and stores in a variable and accepts callback function
+const createList  = (callback) => {
+    var todoItem = document.getElementById('input').value
+    callback(todoItem)
+}
+
+
+// This function will add the user input and also add the edit button to the task
+const addTheItem =(newTask, callback) => {
+    var todotask = document.createElement('li')
+    toDoList.append(todotask)
+    todotask.innerHTML = newTask
+    var EditedItem = document.createElement('a')
+    EditedItem.textContent = 'Edit'
+    todotask.append(EditedItem)
+    callback(EditedItem,todotask)
+}
+
+// This function helps us to edit the entered task
+const editButton = (EditedItem,todotask) => {
+    EditedItem.onclick = () => {
+        var newTask = prompt('Enter your new task')
+        todotask.textContent = newTask
+        todotask.append(EditedItem)
+    }
+}
+
+
+// When clicking the add button, the call back functions are called.
+addButton.onclick = () => {
+    createList((todoItem) =>{
+          addTheItem(todoItem, editButton)
+    })
+}
+
+
+
